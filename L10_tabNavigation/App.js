59fa43e0be76825d225c498de4c2c692ca4d3a7d
DoorlessLlama71 <113698791+DoorlessLlama71@users.noreply.github.com>
2024-04-15 11:54:54 -0500

    Image Picker WOrks

--- L10_tabNavigation/App.js
+++ L10_tabNavigation/App.js
@@ -1,70 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer} from '@react-navigation/native';
+
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-
-import HomeScreen from './app/screens/HomeScreen';
-import InfoScreen from './app/screens/InfoScreen';
-import PhotoScreen from './app/screens/PhotoScreen';
-
-// Find icons! These are the ones I chose
-// import { Ionicons } from '@expo/vector-icons';
-
-// home:
-// <Ionicons name="home" size={24} color="black" />
-// <Ionicons name="home-outline" size={24} color="black" />
-
-// info:
-// <Ionicons name="information-circle" size={24} color="black" />
-// <Ionicons name="information-circle-outline" size={24} color="black" />
-
-// photo:
-// <Ionicons name="image" size={24} color="black" />
-// <Ionicons name="image-outline" size={24} color="black" />
 
 const Tab = createBottomTabNavigator();
+import { createClient } from 'pexels';
+
+const client = createClient('kxTGGbqirQsB9RPP2H9BBT7tq3xeylE11vnKKfKuhkRNf3XqNMglMMQQ');
 
 export default function App() {
+  const [photos, setPhotos] = useState([]);
+
+  const request = async () => {
+    try {
+      const response = await client.photos.search({ query: 'Arizona Wallpaper', per_page: 1 });
+      setPhotos(response.photos);
+      console.log("Works")
+    } catch (error) {
+      console.error('Error fetching photos:', error);
+    }
+  };
+
+  useEffect(() => {
+    request();
+  }, []);
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName='Home'
-        screenOptions={({route }) => ({
-          tabBarIcon: ({focused, color, size}) => {
-            let iconName;
-            // this will specific which icon to use for each tab
-            // the outline is the 'empty' one for when its NOT selected
-            if (route.name ==='Home') {
-              iconName = focused
-              ? 'home'
-              : 'home-outline';
-            }
-            else if (route.name === 'Info') {
-              iconName = focused
-              ? 'information-circle'
-              : 'information-circle-outline'
-            }
-            else if (route.name === 'Photo') {
-              iconName = focused
-              ? 'image'
-              : 'image-outline'
-            }
-            return <Ionicons name={iconName} size ={26} color='blue'/>
-          },
-          tabBarActiveTintColor: 'blue',
-          tabBarInactiveTintColor: 'grey',
-        })}
-      >
-
-        <Tab.Screen name='Home' component={HomeScreen} />
-        <Tab.Screen name='Info' component={InfoScreen} />
-        <Tab.Screen name='Photo' component={PhotoScreen} />
-
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      {photos.map(photo => (
+        <Image key={photo.id} source={{ uri: photo.src.medium }} style={styles.image} />
+      ))}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  image: {
+    height: '52%',
+    width: '100%',
+    marginTop: 0,
+    marginBottom: 15,
+},
 });
